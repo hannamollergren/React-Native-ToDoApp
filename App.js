b24cb9e1e2056d93da24e6de2a02ee3dc7ad4ccc
Hanna Möllergren <hannamollergren@gmail.com>
2021-02-24 15:34:16 +0100

    added header, list, item comp

--- App.js
+++ App.js
@@ -1,24 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, FlatList} from 'react-native';
+import Header from "./components/header";
+import ToDoItem from "./components/toDoItem";
 
 export default function App() {
 	const [todos, setTodos] = useState([
 		{ text: 'buy coffee', key: '1' },
 		{ text: 'create an app', key: '2' },
 		{ text: 'play on the switch', key: '3' }
-	  ]);
+	]);
+
+	const pressHandler = (key) => {
+		setTodos((prevToDos) => {
+			return prevToDos.filter(todo => todo.key != key)
+		})
+	}
+
+
 
 	return (
 		<View style={styles.container}>
-			{/*HEADER*/}
+			<Header/>
 			<View style={styles.content}>
 				{/*TO DO FORM*/}
 				<View style={styles.list}>
 					<FlatList 
 						data={todos}
 						renderItem={({item}) => (
-							<Text>{item.text}</Text>
+							<ToDoItem item={item} pressHandler={pressHandler}/>
 						)}
 					/>
 				</View>
